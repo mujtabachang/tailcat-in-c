7df0e9f823b984f0f98b4e252b3f0732e79cd378
Ahmed Mujtaba <mujtabachang@gmail.com>
2026-09-17 23:11:19 +0500

    test: cover native SSH ProxyCommand construction

---
+++ tests/ssh_command_test.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) Tailscale Inc & contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "tailcat/ssh_command.hpp"
+
+#include <cassert>
+#include <string>
+
+int main() {
+  const std::string address = "tc0123456789abcdefghijklmnopqrstuvwxyz";
+  const auto host1 = tailcat::ssh_destination_host(address);
+  const auto host2 = tailcat::ssh_destination_host(address);
+  assert(host1 == host2);
+  assert(host1.rfind("tailcat-", 0) == 0);
+  assert(host1.size() == std::string("tailcat-").size() + 16U);
+
+  const auto proxy = tailcat::ssh_proxy_command("/tmp/tail cat", address, "22", true);
+  assert(proxy.find(address) != std::string::npos);
+  assert(proxy.find("22") != std::string::npos);
+  assert(proxy.find("--verbose") != std::string::npos);
+#ifdef _WIN32
+  assert(proxy.find("\"/tmp/tail cat\"") != std::string::npos);
+#else
+  assert(proxy.find("'/tmp/tail cat'") != std::string::npos);
+#endif
+  return 0;
+}
