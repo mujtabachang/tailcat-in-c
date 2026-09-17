8bb6accf6543d9174b6867e8165bd4f144ba3bed
Ahmed Mujtaba <mujtabachang@gmail.com>
2026-09-17 23:01:59 +0500

    cli: add forward command interface

---
+++ include/tailcat/forward_command.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) Tailscale Inc & contributors
+// SPDX-License-Identifier: BSD-3-Clause
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace tailcat {
+
+int run_forward_command(const std::vector<std::string>& args, bool verbose);
+
+}  // namespace tailcat
